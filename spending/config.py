828c70c6f2840082d5bf3927a2b349c65f1d1fbb
ku113p <ku113p@gmail.com>
2025-07-05 02:33:56 +0800

    ref - mongo config

--- spending/config.py
+++ spending/config.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config:
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://root:example@localhost:28017")
-    MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "mydb")
-    MONGO_COLLECTION_NAME = os.environ.get("MONGO_COLLECTION_NAME", "mycollection")
+    class Mongo:
+        URI = os.environ.get("MONGO_URI", "mongodb://root:example@localhost:28017")
+        DB_NAME = os.environ.get("MONGO_DB_NAME", "mydb")
+        COLLECTION_NAME = os.environ.get("MONGO_COLLECTION_NAME", "mycollection")
--- spending/db/mongo.py
+++ spending/db/mongo.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from pymongo import AsyncMongoClient
 from pymongo.asynchronous.database import AsyncDatabase
 from pymongo.asynchronous.collection import AsyncCollection
-from typing import AsyncGenerator, AsyncIterable, AsyncIterator
+from typing import AsyncGenerator, AsyncIterable
 
 import utils
 
@@ -16,31 +16,31 @@ logger = utils.create_logger(__name__)
 
 @asynccontextmanager
 async def _get_client() -> AsyncGenerator[AsyncMongoClient, None]:
-    async with AsyncMongoClient(Config.MONGO_URI) as client:
+    async with AsyncMongoClient(Config.Mongo.URI) as client:
         yield client
 
 
 @asynccontextmanager
 async def _get_db() -> AsyncGenerator[AsyncDatabase, None]:
     async with _get_client() as client:
-        yield client[Config.MONGO_DB_NAME]
+        yield client[Config.Mongo.DB_NAME]
 
 
 @asynccontextmanager
 async def _get_collection() -> AsyncGenerator[AsyncCollection, None]:
     async with _get_db() as db:
-        yield db[Config.MONGO_COLLECTION_NAME]
+        yield db[Config.Mongo.COLLECTION_NAME]
 
 
 @register_operation(db_op=DbOperation(db=DbType.MONGO, operation=OperationType.INIT), schema_cls=None)
 async def init_db():
     async with _get_db() as db:
         collections = await db.list_collection_names()
-        if Config.MONGO_COLLECTION_NAME not in collections:
-            await db.create_collection(Config.MONGO_COLLECTION_NAME)
-            logger.info(f"Collection '{Config.MONGO_COLLECTION_NAME}' created.")
+        if Config.Mongo.COLLECTION_NAME not in collections:
+            await db.create_collection(Config.Mongo.COLLECTION_NAME)
+            logger.info(f"Collection '{Config.Mongo.COLLECTION_NAME}' created.")
         else:
-            logger.info(f"Collection '{Config.MONGO_COLLECTION_NAME}' already exists.")
+            logger.info(f"Collection '{Config.Mongo.COLLECTION_NAME}' already exists.")
 
 
 class CreateParams(BaseModel):
@@ -64,7 +64,7 @@ async def list_documents(params: FilterParams) -> AsyncIterable[dict]:
         async with _get_collection() as collection:
             async for doc in collection.find(params.filter):
                 yield doc
-                
+
     return doc_generator()
 
 
